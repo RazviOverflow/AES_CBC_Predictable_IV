5e3074bda2f641ae61955342a05f0929cd81c8b4
RazviOverflow <razvioverflow@gmail.com>
2019-11-26 16:52:38 -0500

    Initial commit

---
+++ code.py
@@ -0,0 +1,49 @@
+from flask import Flask, session, request
+from Crypto import Random
+from Crypto.Cipher import AES
+from secret import flag, aes_key, secret_key
+
+app = Flask(__name__)
+app.secret_key = secret_key
+app.config['DEBUG'] = False
+
+class AESCipher():
+    def __init__(self):
+        self.pad = lambda s: s + (AES.block_size - len(s) % AES.block_size) * chr(AES.block_size - len(s) % AES.block_size)
+
+    def encrypt(self, raw):
+        cipher = AES.new(aes_key, AES.MODE_CBC, session.get("IV"))
+        encrypted = cipher.encrypt(self.pad(raw + flag))
+        session["IV"] = encrypted[-AES.block_size:]
+        return encrypted.encode("hex")
+
+@app.before_request
+def before_request():
+    if session.get("IV") is None:
+        session["IV"] = Random.new().read(AES.block_size)
+
+@app.route("/")
+def main():
+    return "
+
+%s
+
+" % open(__file__).read()
+
+@app.route("/flag_format")
+def flag_format():
+    return """The flag format for this challenge is non-standard.
+
+        The flag to obtain for this challenge (stored in the flag variable) is 32-HEX only.
+
+        Once you obtain this flag, submit your solution in the regular 247CTF{32-HEX} format."""
+
+@app.route("/encrypt")
+def encrypt():
+    try:
+        return AESCipher().encrypt(request.args.get('plaintext').decode('hex'))
+    except:
+        return "Something went wrong!"
+
+if __name__ == "__main__":
+    app.run()
---
+++ script.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python
+
+# from pwn import *
+import requests
+import base64
+import json
+import sys
+
+url = "https://66738677adbf0576.247ctf.com/" # SET THE HOST
+service = "/encrypt?plaintext="
+initial_padding = ("A"*31).encode("hex")
+#flag_begin = "247CTF{"
+possible_letters = ['a', 'b', 'c', 'd', 'e', 'f', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+byte_to_find = ''
+flag_so_far = ''
+
+IV = ''
+
+#### METHOD TO CHECK IV FROM ACTUAL REQUEST COOKIES####
+def get_IV_from_cookies(cookies_dict):
+	cookie_json = json.dumps(cookies_dict)
+	cookie_json = json.loads(cookie_json)
+	cookie_json = cookie_json['session']
+	#print "[+] COOKIES:" + cookie_json
+	cookie_json = cookie_json.split(".")[0]
+	padding = len(cookie_json) % 4
+	cookie_json = cookie_json + "="*padding
+	cookie_json = base64.b64decode(cookie_json)
+	#print "[+] COOKIES: b64: " + cookie_json
+	cookie_json = json.loads(cookie_json)
+	cookie_json = cookie_json['IV']
+	cookie_json = json.dumps(cookie_json)
+	cookie_json = json.loads(cookie_json)
+	cookie_json = cookie_json[' b']
+	#print "[+] COOKIES: b64: " + cookie_json
+	aux_IV = base64.b64decode(cookie_json).encode("hex")
+	#print "[+] COOKIES: b64: INITIALIZATION_VECTOR=== > " + aux_IV
+	return aux_IV
+
+def modify_global_iv(string):
+	global IV
+	IV = string
+	print "[+] IV GLOBAL SET TO: " + IV.encode("hex")
+
+# Return hex already
+def xor_two_str(a,b):
+    xored = []
+    print "XORING " + a.encode("hex") + "\nWITH   " + b.encode("hex") + "\n"
+    for i in range(len(a)):
+        xored_value = ord(a[i%len(a)]) ^ ord(b[i%len(b)])
+        hex_value = hex(xored_value)[2:]
+        if len(hex_value) == 1:
+        	hex_value = "0" + hex_value
+        xored.append(hex_value)
+
+    print "RESULT OF XOR: " + ''.join(xored)
+    return ''.join(xored)
+
+def get_initial_IV(padding):
+	result = requests.request('GET', url)
+	IV_from_cookies = get_IV_from_cookies(result.cookies.get_dict())
+	print "[+] IV FROM COOKIES:" + IV_from_cookies
+	#result = str(wget(url+service+padding))
+	#print "[+] RESULT:" + result.text
+	modify_global_iv(IV_from_cookies.decode("hex"))
+
+def request_cipher_text_and_get_IV(padding):
+	print "Sending padding:" + padding + " length: " + str(len(padding))
+	padding = xor_two_str(IV, padding[:32].decode("hex")) + padding[32:]
+	result = requests.request('GET', url+service+padding)
+	print "Result: " + result.text
+	print "[+] Result to look after:" + result.text[32:63] # 0-31 (first 16 bytes) . 32 - 63 (second 16 bytes) . 64 - 97 (third 16 bytes)
+	global byte_to_find
+	byte_to_find = result.text[32:63]# Insted of comparing last byte, all 32 bytes must be compared
+	modify_global_iv(result.text[-32:].decode("hex"))
+
+
+
+get_initial_IV(initial_padding)
+request_cipher_text_and_get_IV(initial_padding)
+
+local_padding = initial_padding
+while True:
+	for letter in possible_letters:
+
+		print "####### \n\nSending request with IV:" + IV.encode("hex")
+		print "Trying letter:" + letter
+		xored_local_padding = xor_two_str(IV, local_padding[:32].decode("hex"))
+
+		local_padding_to_send =  xored_local_padding + local_padding[32:]
+		print "++++++++++++++++++++++++++++++++++++++++++ length: " + str(len(local_padding_to_send))
+		print "[+] SENDING REQUEST TO:" + url+service+local_padding_to_send + flag_so_far.encode("hex") + letter.encode("hex")
+		local_result = requests.request('GET', url+service+local_padding_to_send + flag_so_far.encode("hex") + letter.encode("hex"))
+		print "[+]RESULT:" + local_result.text
+
+		print "############################### RESULT WE LOOK AFTER: " + byte_to_find
+		print "############################### RESULT WE       HAVE: " + local_result.text[32:63]
+
+		if local_result.text[32:63] == byte_to_find:
+			print "BYTE FOUND! for the letter:" + letter 
+			global flag_so_far
+			flag_so_far = flag_so_far + letter
+			local_padding = local_padding[0:len(local_padding)-2]
+			request_cipher_text_and_get_IV(local_padding)
+			break
+		else:
+			modify_global_iv(local_result.text[-32:].decode("hex"))
+			#print "EL IV DE LAS COOKIES ES: " + get_IV_from_cookies(local_result.cookies.get_dict())
+			#print "################# XLOCAL_IV: " + local_IV
+			#print "################# LENGTH: " + str(len(local_IV))
+	if len(flag_so_far) == 32:
+		break
+
+print "Flag:" + flag_so_far + "}"
