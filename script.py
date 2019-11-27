75c00056e36026ea1da92326d10a3e2c3f2c00be
RazviOverflow <razvioverflow@gmail.com>
2019-11-27 10:22:01 +0100

    Code cleaned Changed Author

--- script.py
+++ script.py
@@ -10,7 +10,7 @@ url = "" # SET THE HOST
 service = "/encrypt?plaintext="
 initial_padding = ("A"*47).encode("hex")
 #flag_begin = "247CTF{"
-possible_letters = ['a', 'b', 'c', 'd', 'e', 'f', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+possible_hex_digits = ['a', 'b', 'c', 'd', 'e', 'f', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 byte_to_find = ''
 flag_so_far = ''
 
@@ -22,7 +22,6 @@ request_session = requests.Session()
 def get_IV_from_cookies(cookies_dict):
 	cookie_json = json.dumps(cookies_dict)
 	cookie_json = json.loads(cookie_json)
-	print "WHAt????????????????'''" + str(cookie_json)
 	cookie_json = cookie_json['session']
 	#print "[+] COOKIES:" + cookie_json
 	cookie_json = cookie_json.split(".")[0]
@@ -40,76 +39,68 @@ def get_IV_from_cookies(cookies_dict):
 	#print "[+] COOKIES: b64: INITIALIZATION_VECTOR=== > " + aux_IV
 	return aux_IV
 
-def modify_global_iv(string):
-	global IV
-	IV = string
-	print "[+] IV GLOBAL SET TO: " + IV.encode("hex")
-
-# Return hex already
+# Returns hex already
 def xor_two_str(a,b):
     xored = []
-    print "XORING " + a.encode("hex") + "\nWITH   " + b.encode("hex") + "\n"
     for i in range(len(a)):
         xored_value = ord(a[i%len(a)]) ^ ord(b[i%len(b)])
         hex_value = hex(xored_value)[2:]
         if len(hex_value) == 1:
         	hex_value = "0" + hex_value
         xored.append(hex_value)
-
-    print "RESULT OF XOR: " + ''.join(xored)
     return ''.join(xored)
 
 def get_initial_IV(padding):
 	result = request_session.request('GET', url)
 	IV_from_cookies = get_IV_from_cookies(result.cookies.get_dict())
-	print "[+] IV FROM COOKIES:" + IV_from_cookies
-	#result = str(wget(url+service+padding))
-	#print "[+] RESULT:" + result.text
 	modify_global_iv(IV_from_cookies.decode("hex"))
 
 def request_cipher_text_and_get_IV(padding):
-	print "Sending padding:" + padding + " length: " + str(len(padding))
+	print "[+] SENDING REQUEST TO:" + url+service+padding
 	padding = xor_two_str(IV, padding[:32].decode("hex")) + padding[32:]
 	result = request_session.request('GET', url+service+padding)
-	print "Result: " + result.text
-	print "[+] Result to look after:" + result.text[64:96] # 0-31 (first 16 bytes) . 32 - 63 (second 16 bytes) . 64 - 97 (third 16 bytes)
+	print "[+] RESULT TO LOOK AFTER:" + result.text[64:96] # 0-31 (first 16 bytes) . 32 - 63 (second 16 bytes) . 64 - 97 (third 16 bytes)
 	global byte_to_find
 	byte_to_find = result.text[64:96]# Insted of comparing last byte, all 32 bytes must be compared
 	modify_global_iv(result.text[-32:].decode("hex"))
 
+def modify_global_iv(string):
+	global IV
+	IV = string
+	print "[+] IV GLOBAL SET TO: " + IV.encode("hex")
 
+if __name__ == "__main__":
 
-get_initial_IV(initial_padding)
-request_cipher_text_and_get_IV(initial_padding)
+	# Get the first random IV
+	get_initial_IV(initial_padding)
 
-local_padding = initial_padding
-while True:
-	
-	for letter in possible_letters:
+	# Get the first ciphertext to leak
+	request_cipher_text_and_get_IV(initial_padding)
 
-		print "####### \n\nSending request with IV:" + IV.encode("hex")
-		print "Trying letter:" + letter
-		xored_local_padding = xor_two_str(IV, local_padding[:32].decode("hex"))
+	local_padding = initial_padding
 
-		local_padding_to_send =  xored_local_padding + local_padding[32:]
-		print "++++++++++++++++++++++++++++++++++++++++++ length: " + str(len(local_padding_to_send))
-		print "[+] SENDING REQUEST TO:" + url+service+local_padding_to_send + flag_so_far.encode("hex") + letter.encode("hex")
-		local_result = request_session.request('GET', url+service+local_padding_to_send + flag_so_far.encode("hex") + letter.encode("hex"))
-		print "[+]RESULT:" + local_result.text
-		modify_global_iv(local_result.text[-32:].decode("hex"))
-		print "############################### RESULT WE LOOK AFTER: " + byte_to_find
-		print "############################### RESULT WE       HAVE: " + local_result.text[64:96]
+	# Iterate leaking bytes until leaked information (flag_so_far) has correct length
+	while True:
+		for hex_digit in possible_hex_digits:
+			xored_local_padding = xor_two_str(IV, local_padding[:32].decode("hex"))
+			local_padding_to_send =  xored_local_padding + local_padding[32:]
+			print "[+] SENDING REQUEST TO:" + url+service+local_padding_to_send + flag_so_far.encode("hex") + hex_digit.encode("hex")
+			local_result = request_session.request('GET', url+service+local_padding_to_send + flag_so_far.encode("hex") + hex_digit.encode("hex"))
+			print "[+] RESULT:" + local_result.text
+			modify_global_iv(local_result.text[-32:].decode("hex"))
+			print "[+] LOOKING FOR: " + byte_to_find
+			print "[+] COMPARING  : " + local_result.text[64:96]
 
-		if local_result.text[64:96] == byte_to_find:
-			print "BYTE FOUND! for the letter:" + letter 
-			global flag_so_far
-			flag_so_far = flag_so_far + letter
-			print "[!][!][!][!][!][!] Flag so far:" + flag_so_far
-			local_padding = local_padding[0:len(local_padding)-2]
-			request_cipher_text_and_get_IV(local_padding)
+			if local_result.text[64:96] == byte_to_find:
+				print "[+][+][+] BYTE FOUND!\n [+][+][+] LEAKED HEX DIGIT:" + hex_digit 
+				global flag_so_far
+				flag_so_far = flag_so_far + hex_digit
+				print "[!] FLAG SO FAR (32-HEX ONLY):" + flag_so_far
+				local_padding = local_padding[0:len(local_padding)-2]
+				request_cipher_text_and_get_IV(local_padding)
+				break
+			
+		if len(flag_so_far) == 32:
 			break
-		
-	if len(flag_so_far) == 32:
-		break
 
-print "Flag:" + flag_so_far + "}"
+	print "Flag:247CTF{" + flag_so_far + "}"
