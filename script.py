c4ebb83ff3a5288d652c417b2087d17192b9b6f8
RazviOverflow <razvioverflow@gmail.com>
2019-11-26 19:24:38 -0500

    Flag obtained. Time to clean the code

--- script.py
+++ script.py
@@ -6,9 +6,9 @@ import base64
 import json
 import sys
 
-url = "https://66738677adbf0576.247ctf.com/" # SET THE HOST
+url = "" # SET THE HOST
 service = "/encrypt?plaintext="
-initial_padding = ("A"*31).encode("hex")
+initial_padding = ("A"*47).encode("hex")
 #flag_begin = "247CTF{"
 possible_letters = ['a', 'b', 'c', 'd', 'e', 'f', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 byte_to_find = ''
@@ -22,6 +22,7 @@ request_session = requests.Session()
 def get_IV_from_cookies(cookies_dict):
 	cookie_json = json.dumps(cookies_dict)
 	cookie_json = json.loads(cookie_json)
+	print "WHAt????????????????'''" + str(cookie_json)
 	cookie_json = cookie_json['session']
 	#print "[+] COOKIES:" + cookie_json
 	cookie_json = cookie_json.split(".")[0]
@@ -71,9 +72,9 @@ def request_cipher_text_and_get_IV(padding):
 	padding = xor_two_str(IV, padding[:32].decode("hex")) + padding[32:]
 	result = request_session.request('GET', url+service+padding)
 	print "Result: " + result.text
-	print "[+] Result to look after:" + result.text[32:63] # 0-31 (first 16 bytes) . 32 - 63 (second 16 bytes) . 64 - 97 (third 16 bytes)
+	print "[+] Result to look after:" + result.text[64:96] # 0-31 (first 16 bytes) . 32 - 63 (second 16 bytes) . 64 - 97 (third 16 bytes)
 	global byte_to_find
-	byte_to_find = result.text[32:63]# Insted of comparing last byte, all 32 bytes must be compared
+	byte_to_find = result.text[64:96]# Insted of comparing last byte, all 32 bytes must be compared
 	modify_global_iv(result.text[-32:].decode("hex"))
 
 
@@ -83,6 +84,7 @@ request_cipher_text_and_get_IV(initial_padding)
 
 local_padding = initial_padding
 while True:
+	
 	for letter in possible_letters:
 
 		print "####### \n\nSending request with IV:" + IV.encode("hex")
@@ -94,22 +96,19 @@ while True:
 		print "[+] SENDING REQUEST TO:" + url+service+local_padding_to_send + flag_so_far.encode("hex") + letter.encode("hex")
 		local_result = request_session.request('GET', url+service+local_padding_to_send + flag_so_far.encode("hex") + letter.encode("hex"))
 		print "[+]RESULT:" + local_result.text
-
+		modify_global_iv(local_result.text[-32:].decode("hex"))
 		print "############################### RESULT WE LOOK AFTER: " + byte_to_find
-		print "############################### RESULT WE       HAVE: " + local_result.text[32:63]
+		print "############################### RESULT WE       HAVE: " + local_result.text[64:96]
 
-		if local_result.text[32:63] == byte_to_find:
+		if local_result.text[64:96] == byte_to_find:
 			print "BYTE FOUND! for the letter:" + letter 
 			global flag_so_far
 			flag_so_far = flag_so_far + letter
+			print "[!][!][!][!][!][!] Flag so far:" + flag_so_far
 			local_padding = local_padding[0:len(local_padding)-2]
 			request_cipher_text_and_get_IV(local_padding)
 			break
-		else:
-			modify_global_iv(local_result.text[-32:].decode("hex"))
-			#print "EL IV DE LAS COOKIES ES: " + get_IV_from_cookies(local_result.cookies.get_dict())
-			#print "################# XLOCAL_IV: " + local_IV
-			#print "################# LENGTH: " + str(len(local_IV))
+		
 	if len(flag_so_far) == 32:
 		break
 
